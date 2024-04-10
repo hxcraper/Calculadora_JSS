7f9f6a0283cd3ce31dcbf0e9f61124b62d7845ab
hxcraper <hxcraper@gmail.com>
2024-04-09 23:57:56 -0400

    V1

---
+++ index.js
@@ -0,0 +1,40 @@
+
+console.log("wena culiaito")
+// tipos de datos
+let edad = 30;
+let texto = "mi edad es " + edad;
+console.log(texto);
+
+// se puede modificar
+let miVariable = 30;
+miVariable = 12;
+console.log(miVariable)
+
+// const no se puede modificar luego
+const variableConst = 30;
+// variableConst = 12;
+console.log(variableConst);
+
+
+let op1 = 30, op2 = 20;
+
+console.log("La suma es: " + (op1 + op2));
+console.log("la resta es: " + (op1 - op2));
+
+// condicionales,
+
+// IF
+let miNumero = 6;
+if(miNumero === "6"){ // === hace que sea estricto, en este caso, es falso ya que no son del mismo tipo
+    console.log("Si, mi numero vale 6");
+} else{
+    console.log("No, mi numero no es 6")
+};
+
+
+
+
+
+
+
+
